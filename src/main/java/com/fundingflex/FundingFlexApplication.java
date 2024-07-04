4f4e6ad319ed050e2dc2db37d7fd7477473c44c2
rapier4231 <123789153+rapier4231@users.noreply.github.com>
2024-07-04 16:48:42 +0900

    Revert "[강훈 - Pay 머지 ]Feat kh pay"

--- src/main/java/com/fundingflex/FundingFlexApplication.java
+++ src/main/java/com/fundingflex/FundingFlexApplication.java
@@ -1,27 +1,13 @@
 package com.fundingflex;
 
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import com.fundingflex.pay.sender.KakaoPayRequestSender;
-
-//@SpringBootApplication(exclude = SecurityAutoConfiguration.class) //스프링 시큐리티 비활성화
 @SpringBootApplication
-@MapperScan("com.fundingflex.mybatis.mapper")
 public class FundingFlexApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FundingFlexApplication.class, args);
 	}
 
-//    @Bean
-//    public CommandLineRunner run(KakaoPayRequestSender kakaoPayRequestSender) {
-//        return args -> {
-//            kakaoPayRequestSender.testSend();
-//        };
-//    }
-//    
 }
--- src/main/java/com/fundingflex/member/domain/dto/MemberSigninRequest.java
+++
@@ -1,9 +0,0 @@
-package com.fundingflex.member.domain.dto;
-
-import lombok.Data;
-
-@Data
-public class MemberSigninRequest {
-	private String email;
-    private String password;
-}
