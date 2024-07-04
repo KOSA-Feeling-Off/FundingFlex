d4a9005847c1fb1824bfead5cc0919d1096727d9
DESKTOP-4U4G4G2\KOSA <dmswjd4566@hanmail.net>
2024-07-04 18:51:58 +0900

    Feat: 알림 테스트함
    - 알림 테스트 시 처음은 잘 돌아감
    - 다시 했을 경우 시큐리티에서 문제가 생기는 듯함 -> 확인 필요!!
    
    - Consultations 테이블과 컬럼명 맞춤

--- src/main/java/com/fundingflex/consultation/qa/domain/entity/Consultations.java
+++ src/main/java/com/fundingflex/consultation/qa/domain/entity/Consultations.java
@@ -34,13 +34,10 @@ public class Consultations {
     @Column(name = "created_at", nullable = false)
     private Date createdAt;
 
-    @Column(name = "is_reply", columnDefinition = "CHAR(1) DEFAULT 'N'")
-    private String isReply;
-
-    @Column(name = "members_user_id", nullable = false)
+    @Column(name = "user_id", nullable = false)
     private Long membersUserId;
 
-    @Column(name = "admin_user_id", columnDefinition = "NUMBER(19, 0)")
+    @Column(name = "admin_id", columnDefinition = "NUMBER(19, 0)")
     @ColumnDefault("NULL")
     private Long adminUserId;
 
